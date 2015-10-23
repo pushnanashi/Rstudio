1f290500904dd51deeb23d31f259afcdcd560e4a
pushnanashi <relomeru@yahoo.co.jp>
2015-10-24 05:10:40 +0900

    rpart

--- Spam_Mashine_Learning/program/count.pl
+++ Spam_Mashine_Learning/program/count.pl
@@ -33,14 +33,14 @@ open(DATAFILE, "< $fileuri")or die("error :$!");
 @splittime;
 @htmlnum;
 @text;
-@non-multipart;
+@nonmultipart;
 @multipart_text;
-@multipart_non-text;
+@multipart_nontext;
 @spliturl;
 @spliturlper;
 @splitspam_word;
 @splitspam_f;
-@splitnon-spam;
+@splitnonspam;
 @splitspam;
 
 
@@ -55,15 +55,18 @@ if ($line =~ /4/) {
 
     ($a1,$a2,$a3,$a4,$a5,$a6,$a7,$a8,$a9,$a10,$a11,$a12) = split(",",$line);
 
+
+
+
     push(@ip,$a1); 
     push(@time,$a2);
     push(@msid,$a3);
     push(@subject,$a4);
-    push(@text,$a5);
-    push(@multi,$a6);
-    push(@url,$a7);
-    push(@urlper,$a8);
-    push(@time,$a9);
+    push(@from,$a5);
+    push(@text,$a6);
+    push(@multi,$a7);
+    push(@url,$a8);
+    push(@urlper,$a9);
     push(@spam_word,$a10);
     push(@spam_f,$a11);
     push(@spam,$a12);
@@ -79,11 +82,11 @@ if ($line =~ /4/) {
 #1228118400 <-2008/12/01 17:00:00  1267426162<-2010/03/01 15:49:22   (1228118400 - 1267455600)を時間間隔で検索
 
 
-for(i=1228118400;i<=1267455600;i+=3600){
-    $ii = $i + 3600;
-    $patterm = "[$i-$ii]";
+for($z=1228118400;$z<=1267455600;$z+=3600){
+    $ii = $z + 3600;
+    $pattern = "[$z-$ii]";
 
-    push(@splittime,$i;);
+    push(@splittime,$z);
     
     $b1=0;
     $b2=0;
@@ -102,19 +105,19 @@ for(i=1228118400;i<=1267455600;i+=3600){
     for($i=0;$i<=$#time;$i++){
 
 	#time範囲
-	if ($time =~ /$pattern/) {
+	if ($time[$i] =~ /$pattern/) {
 	    
 	  if ($text[$i] =~ /HTML/) {
 	      $b1++;
-	  }else if($text[$i] =~ /text/){
+	  }elsif($text[$i] =~ /text/){
 	      $b2++;
 	  }
 	    
 	  if ($multi[$i] =~ /non-multipart/) {
               $b3++;
-          }else if($multi[$i] =~ /multipart_text/){
+          }elsif($multi[$i] =~ /multipart_text/){
               $b4++;
-          }else if($multi[$i] =~ /multipart_non-text/){
+          }elsif($multi[$i] =~ /multipart_non-text/){
               $b5++;
           }
 
@@ -127,7 +130,7 @@ for(i=1228118400;i<=1267455600;i+=3600){
           $b9+=$spam_f[$i];   
 	  if($multi[$i] =~ /non-spam/){
               $b10++;
-          }else if($multi[$i] =~ /spam/){
+          }elsif($multi[$i] =~ /spam/){
               $b11++;
           }
 
@@ -139,17 +142,18 @@ for(i=1228118400;i<=1267455600;i+=3600){
 
     }
 
-		 push(@htmlnum,$b1);
-		 push(@text,$b2);
-		 push(@non-multipart,$b3);
-		 push(@multipart_text,$b4;
-		 push(@multipart_non-text,$b5);
-		 push(@spliturl,$b6);
-		 push(@spliturlper,$7);
-		 push(@splitspam_word,$b8);
-		 push(@splitspam_f,$b9);
-		 push(@splitnon-spam,$b10);
-	         push(@splitspam-spam,$b11);
+	
+    push(@htmlnum,$b1);
+    push(@text,$b2);
+    push(@nonmultipart,$b3);
+    push(@multipart_text,$b4);
+	 push(@multipart_nontext,$b5);
+	 push(@spliturl,$b6);
+	 push(@spliturlper,$b7);
+	 push(@splitspam_word,$b8);
+	 push(@splitspam_f,$b9);
+	 push(@splitnonspam,$b10);
+	 push(@splitspamspam,$b11);
 
    
   
@@ -161,7 +165,7 @@ for(i=1228118400;i<=1267455600;i+=3600){
 
     for($i=0;$i<=$#splittime;$i++){
 
-      print ECHO "$splittime[$i],$html1um[$i],$text[$i],$non-multipart[$i],$multipart_text[$i],$multipart_non-text[$i],$spliturl[$i],$spliturlper[$i],$splitspam_word[$i],$splitspam_f[$i],$splitnon-spam[$i],$splitspam-spam[$i]\n";
+      print ECHO "$splittime[$i],$html1um[$i],$text[$i],$nonmultipart[$i],$multipart_text[$i],$multipart_nontext[$i],$spliturl[$i],$spliturlper[$i],$splitspam_word[$i],$splitspam_f[$i],$splitnonspam[$i],$splitspamspam[$i]\n";
 
 
 
